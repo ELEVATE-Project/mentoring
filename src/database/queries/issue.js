9f058458ffe6d7c0ca527f6e472f6c748542841e
sumanvpacewisdom <suman.v@pacewisdom.com>
2025-07-29 21:41:34 +0530

    Issues and ManageSession Module

--- src/database/queries/issue.js
+++ src/database/queries/issue.js
@@ -1,7 +1,7 @@
 const Issue = require('../models/index').Issue
 
 module.exports = class issueData {
-	static async create(data) {
+	static async create(data, tenantCode) {
 		try {
 			return await Issue.create(data, { returning: true })
 		} catch (error) {
