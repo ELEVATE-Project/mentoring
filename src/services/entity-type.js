4ab8cb6abdeaf2e0a37eaa206803085d9e27c4b5
Rocky <rakesh.rockysgr@gmail.com>
2025-08-19 19:51:27 +0530

    changes

--- src/services/entity-type.js
+++ src/services/entity-type.js
@@ -278,7 +278,7 @@ module.exports = class EntityHelper {
 			}
 			if (entityType) filter.value = entityType
 			// get entityTypes with entities data
-			let entityTypesWithEntities = await entityTypeQueries.findUserEntityTypesAndEntities(filter, tenantCode)
+			let entityTypesWithEntities = await entityTypeQueries.findUserEntityTypesAndEntities(filter, tenantCodes)
 			entityTypesWithEntities = JSON.parse(JSON.stringify(entityTypesWithEntities))
 			if (!entityTypesWithEntities.length > 0) {
 				return responseData
