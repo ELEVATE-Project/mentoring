d0ab964e8758c1ed41d988fce14f3887341f82f9
sumanvpacewisdom <suman.v@pacewisdom.com>
2025-09-02 17:27:58 +0530

    Fix : adding tenant code for communications

--- src/helpers/communications.js
+++ src/helpers/communications.js
@@ -13,9 +13,9 @@ const userRequests = require('@requests/user')
  * @returns {Promise<Object>} An object containing auth_token and user_id if login is successful.
  * @throws Will throw an error if the login request fails for reasons other than unauthorized access.
  */
-exports.login = async (userId) => {
+exports.login = async (userId, tenantCode) => {
 	try {
-		const login = await communicationRequests.login({ userId })
+		const login = await communicationRequests.login({ userId, tenantCode })
 		return {
 			auth_token: login.result.auth_token,
 			user_id: login.result.user_id,
@@ -35,9 +35,9 @@ exports.login = async (userId) => {
  * @returns {Promise<Object>} The status of the logout operation.
  * @throws Will throw an error if the logout request fails for reasons other than unauthorized access.
  */
-exports.logout = async (userId) => {
+exports.logout = async (userId, tenantCode) => {
 	try {
-		const logout = await communicationRequests.logout({ userId })
+		const logout = await communicationRequests.logout({ userId, tenantCode })
 		return logout.result.status
 	} catch (error) {
 		if (error.message === common.COMMUNICATION.UNAUTHORIZED) {
@@ -55,9 +55,9 @@ exports.logout = async (userId) => {
  * @returns {Promise<void>} Resolves if the update is successful.
  * @throws Will throw an error if the updateAvatar request fails.
  */
-exports.updateAvatar = async (userId, imageUrl) => {
+exports.updateAvatar = async (userId, imageUrl, tenantCode) => {
 	try {
-		await communicationRequests.updateAvatar(userId, imageUrl)
+		await communicationRequests.updateAvatar(userId, imageUrl, tenantCode)
 	} catch (error) {
 		console.error(`Error updating avatar for user ${userId}:`, error.message)
 		throw error
@@ -72,9 +72,9 @@ exports.updateAvatar = async (userId, imageUrl) => {
  * @returns {Promise<void>} Resolves if the update is successful.
  * @throws Will throw an error if the updateUser request fails.
  */
-exports.updateUser = async (userId, name) => {
+exports.updateUser = async (userId, name, tenantCode) => {
 	try {
-		await communicationRequests.updateUser(userId, name)
+		await communicationRequests.updateUser(userId, name, tenantCode)
 	} catch (error) {
 		console.error(`Error updating user ${userId}:`, error.message)
 		throw error
@@ -106,7 +106,7 @@ exports.createOrUpdateUser = async ({ userId, name, email, image, tenantCode }) 
 		if (user && user.meta?.communications_user_id) {
 			// Update user information if already exists in the communication service
 			await Promise.all([
-				image ? this.updateAvatar(userId, image) : Promise.resolve(),
+				image ? this.updateAvatar(userId, image, tenantCode) : Promise.resolve(),
 				name ? this.updateUser(userId, name, tenantCode) : Promise.resolve(),
 			])
 		} else {
@@ -192,6 +192,7 @@ exports.createChatRoom = async (recipientUserId, initiatorUserId, initialMessage
 		const chatRoom = await communicationRequests.createChatRoom({
 			userIds: [initiatorUserId, recipientUserId],
 			initialMessage: initialMessage,
+			tenantCode: tenantCode,
 		})
 		return chatRoom
 	} catch (error) {
@@ -214,9 +215,9 @@ exports.createChatRoom = async (recipientUserId, initiatorUserId, initialMessage
  * const result = await resolve('external-user-123');
  * // result => { user_id: 'internal-user-456' }
  */
-exports.resolve = async (userId) => {
+exports.resolve = async (userId, tenantCode) => {
 	try {
-		const userIdResponse = await communicationRequests.getUserId(userId)
+		const userIdResponse = await communicationRequests.getUserId(userId, tenantCode)
 
 		return {
 			user_id: userIdResponse.result.user_id,
@@ -247,9 +248,14 @@ exports.resolve = async (userId) => {
  * const wasUpdated = await setActiveStatus('abc123', false, true);
  * // wasUpdated => true if successful
  */
-exports.setActiveStatus = async (userId, activeStatus, confirmRelinquish) => {
+exports.setActiveStatus = async (userId, activeStatus, confirmRelinquish, tenantCode) => {
 	try {
-		const setUserActiveStatus = await communicationRequests.setActiveStatus(userId, activeStatus, confirmRelinquish)
+		const setUserActiveStatus = await communicationRequests.setActiveStatus(
+			userId,
+			activeStatus,
+			confirmRelinquish,
+			tenantCode
+		)
 		return setUserActiveStatus
 	} catch (error) {
 		console.error(`Error updating user ${userId}:`, error.message)
@@ -273,9 +279,9 @@ exports.setActiveStatus = async (userId, activeStatus, confirmRelinquish) => {
  * const result = await removeAvatar('abc123');
  * // result => { result: { success: true }, statusCode: 200, message: 'AVATAR_REMOVED' }
  */
-exports.removeAvatar = async (userId) => {
+exports.removeAvatar = async (userId, tenantCode) => {
 	try {
-		const removeAvatarStatus = await communicationRequests.removeAvatar(userId)
+		const removeAvatarStatus = await communicationRequests.removeAvatar(userId, tenantCode)
 		return removeAvatarStatus
 	} catch (error) {
 		console.error(`Error remove avatar of the user ${userId}:`, error.message)
